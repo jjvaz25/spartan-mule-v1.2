5df9c18b9542ddbfa91942bd5b45429c2ac5817b
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2020-01-16 17:55:09 -0500

    add top navbar layout

--- sm-v1.2/src/App.vue
+++ sm-v1.2/src/App.vue
@@ -1,19 +1,31 @@
 <template>
-  <v-app>
-    <v-content>
+  <v-app id="keep">
+    <top-nav />
+    <v-content
+      class="grey lighten-4 fill-height"
+    >
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import TopNav from '../src/components/TopNav'
 
 export default {
   name: 'App',
+  components: {
+    'top-nav': TopNav
+  },
   data() {
     return {
-      
     }
   }
 };
 </script>
+
+<style>
+/* #keep .v-navigation-drawer__border {
+  display: none
+} */
+</style>
---
+++ sm-v1.2/src/components/TopNav.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app-bar
+    app
+    flat
+    clipped-left
+    color="primary"
+    dark
+  >
+    <v-app-bar-nav-icon large />
+    <span class="headline ml-3 font-weight-regular">Spartan<span class="font-weight-medium">Mule</span></span>
+    <v-spacer></v-spacer>
+    <div>
+      <v-menu
+        offset-y
+        origin="top center"
+        transition="scale-transition"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" text>
+            <v-icon>person</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-icon small left>person</v-icon>
+            <v-list-item-title>My Account</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-icon small left>exit_to_app</v-icon>
+            <v-list-item-title>Sign out</v-list-item-title>
+          </v-list-item>
+        </v-list>
+
+      </v-menu>
+    </div>
+  </v-app-bar>
+
+</template>
+
+<script>
+export default {
+  name: 'TopNav',
+  data() {
+    return {
+
+    }
+  },
+};
+</script>
