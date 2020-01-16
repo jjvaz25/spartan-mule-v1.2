9890044baedfe2a338da43c10174fd5801df5010
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2020-01-16 17:57:53 -0500

    add bottom footer

--- sm-v1.2/src/App.vue
+++ sm-v1.2/src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <v-app id="keep">
     <top-nav />
-    <v-content
-      class="grey lighten-4 fill-height"
-    >
+    <v-content class="grey lighten-4 fill-height">
       <router-view></router-view>
     </v-content>
+    <bottom-footer />
   </v-app>
 </template>
 
 <script>
 import TopNav from '../src/components/TopNav'
+import Footer from '../src/components/Footer'
 
 export default {
   name: 'App',
   components: {
-    'top-nav': TopNav
+    'top-nav': TopNav,
+    'bottom-footer': Footer
   },
   data() {
     return {
