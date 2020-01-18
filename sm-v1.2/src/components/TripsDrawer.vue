ef5afde1453fb9e66dc94807bad6cf14c545cd76
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2020-01-18 17:38:16 -0500

    add ability to delete trip

--- sm-v1.2/src/components/TripsDrawer.vue
+++ sm-v1.2/src/components/TripsDrawer.vue
@@ -6,49 +6,6 @@
       <v-btn text color="white" @click="addTripSnackbar = false">Close</v-btn>
   </v-snackbar>
 
-  <!-- <v-navigation-drawer
-    permanent
-    class="grey lighten-3"
-  >
-    <v-list>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">My Trips</v-list-item-title>
-        </v-list-item-content>
-        <v-btn text><v-icon>add</v-icon></v-btn>
-      </v-list-item>
-    </v-list>
-
-    <v-divider></v-divider>
-
-    <v-list two-line>
-      <v-subheader>Upcoming</v-subheader>
-      <v-list-item
-        v-for="(trip, index) in upcomingTrips" :key="index"
-      >
-        <v-list-item-content>
-          <v-list-item-title>{{ trip.title }}</v-list-item-title>
-          <v-list-item-subtitle>{{ trip.dates }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-
-    <v-divider></v-divider>
-    
-    <v-list two-line>
-      <v-subheader>Past adventures</v-subheader>
-
-      <v-list-item
-        v-for="(trip, index) in pastAdventures" :key="index"
-      >
-        <v-list-item-content>
-          <v-list-item-title>{{ trip.title }}</v-list-item-title>
-          <v-list-item-subtitle>{{ trip.dates }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer> -->
-
   <v-navigation-drawer
     permanent
     class="grey lighten-3"
@@ -108,7 +65,7 @@
               <!-- <v-spacer></v-spacer> -->
               <v-btn text small class="success">Save</v-btn>
               <v-btn @click="trip.isEditing = false" text small class="warning">Cancel</v-btn>
-              <v-btn text small class="error"><v-icon small>delete</v-icon></v-btn>
+              <v-btn @click="deleteTrip(trip.id)" text small class="error"><v-icon small>delete</v-icon></v-btn>
           </v-card-actions>
         </v-card>
         </v-list-item-content>
@@ -158,6 +115,14 @@ export default {
       menu: false,
       dateRange: ['2020-01-29', '2020-02-02'],
       addTripSnackbar: false
+    }
+  },
+  methods: {
+    deleteTrip(id) {
+      db.collection('trips').doc(id).delete()
+      const index = this.trips.findIndex(item => item.id === id)
+      this.trips.splice(index, 1)
+      // this.deleteTripSnackbar = true
     }
   },
   computed: {
